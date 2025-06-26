0ff7fec5039093bc15f16d6b0152b2e200ce8291
Sayani Sen <sayanisen.official@gmail.com>
2025-06-26 11:59:57 +0530

    changes in inscriptions, script, css, fixed the link highlight and indexbtn show scripts

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 
-  $(document).ready(function () {
-    const $overlay = $('#searchOverlay');
-    const $searchBox = $('.search-box');
-    const $input = $overlay.find('input');
+  // $(document).ready(function () {
+  //   const $overlay = $('#searchOverlay');
+  //   const $searchBox = $('.search-box');
+  //   const $input = $overlay.find('input');
 
-    // Show overlay on button click
-    $(document).on('click', '.search-btn', function (e) {
-      e.stopPropagation();
-      $overlay.addClass('active');
-      setTimeout(() => {
-            $searchBox.css('transform', 'translateX(0)'); // Gradual transition applied
-        }, 100);
-    });
+  //   // Show overlay on button click
+  //   $(document).on('click', '.search-btn', function (e) {
+  //     e.stopPropagation();
+  //     $overlay.addClass('active');
+  //     setTimeout(() => {
+  //           $searchBox.css('transform', 'translateX(0)'); // Gradual transition applied
+  //       }, 100);
+  //   });
 
-    // Close overlay on clicking the ✖ icon
-    $(document).on('click', '#closeOverlay',function () {
-      $searchBox.css('transform', 'translateX(-400px)'); // Hide smoothly before removing overlay
-        setTimeout(() => {
-            $overlay.removeClass('active');
-        }, 300); 
-    });
+  //   // Close overlay on clicking the ✖ icon
+  //   $(document).on('click', '#closeOverlay',function () {
+  //     $searchBox.css('transform', 'translateX(-400px)'); // Hide smoothly before removing overlay
+  //       setTimeout(() => {
+  //           $overlay.removeClass('active');
+  //       }, 300); 
+  //   });
 
-    // Prevent showing cursor by default — only when user clicks input    
-      $(document).on('focus', '.custom-caret', function(){
-        $(this).css('caret-color', 'auto');
-      }).on('blur', '.custom-caret', function(){
-        $(this).css('caret-color', 'transparent');
-      });
-      $('.custom-caret').css('caret-color', 'transparent');
-  });
+  //   // Prevent showing cursor by default — only when user clicks input    
+  //     $(document).on('focus', '.custom-caret', function(){
+  //       $(this).css('caret-color', 'auto');
+  //     }).on('blur', '.custom-caret', function(){
+  //       $(this).css('caret-color', 'transparent');
+  //     });
+  //     $('.custom-caret').css('caret-color', 'transparent');
+  // });
 
   /****Navbar hamburger menu for mobile******/
 $(document).ready(function(){
@@ -50,7 +50,7 @@ $(document).ready(function(){
 
     // Optional: hide menu on link click (mobile)
     $('.nav-links a').click(function () {
-      if ($(window).width() < 768) {
+      if ($(window).width() < 991) {
         $('.nav-links').slideUp();
         $('.menu-toggle i').removeClass('fa-times').addClass('fa-bars');
         isOpen = false;
@@ -59,7 +59,7 @@ $(document).ready(function(){
 
     // Reset menu if window resized
     $(window).resize(function () {
-      if ($(window).width() > 768) {
+      if ($(window).width() > 991) {
         $('.nav-links').show();
         $('.menu-toggle i').removeClass('fa-times').addClass('fa-bars');
       } else if (!isOpen) {
